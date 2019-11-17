35b289a830e6159aa714a0a7681a1990f6ce0127
Daniel Goldman <dan@Ozmos-MacBook-Pro.local>
2019-11-16 22:14:44 -0500

    Friend Finder UNCC Assignment

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Friend-Finder
-UNCC Bootcamp FRIEND FINDER Node+Express
+# Express & Node.js Friend Finder
+
+### Concept
+
+This activity is a compatibility-based "FriendFinder" application -- basically a dating app. This full-stack site will take in results from your users' surveys, then compare their answers with those from other users. The app will then display the name and picture of the user with the best overall match.
+
+Express to handles routing. The application is deployed to heroku at the following URL for inspection:
+
+<continue>
+
---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+var friends = [
+    {
+        name : "Roland Black",
+        photo: "https://randomuser.me/api/portraits/men/8.jpg",
+        scores: [5,1,2,4,2,5,3,4,5,1]
+    },
+    {
+        name : "Viola Silva",
+        photo: "https://randomuser.me/api/portraits/women/30.jpg",
+        scores: [5,3,5,5,5,1,4,3,4,1]
+    },
+    {
+        name : "Wayne Jefferson",
+        photo: "https://randomuser.me/api/portraits/men/63.jpg",
+        scores: [1,5,2,4,3,2,3,5,2,3]
+    },
+    {
+        name : "Janice Morrison",
+        photo: "https://randomuser.me/api/portraits/women/43.jpg",
+        scores: [4,2,5,4,2,2,4,5,1,4]
+    },
+    {
+        name : "Cory Richardson",
+        photo: "https://randomuser.me/api/portraits/men/50.jpg",
+        scores: [4,5,4,3,5,1,3,2,5,1]
+    },
+    {
+        name : "Alice Ballard",
+        photo: "https://randomuser.me/api/portraits/women/12.jpg",
+        scores: [5,3,3,4,3,1,3,5,1,4]
+    }
+];
+
+module.exports = friends;
